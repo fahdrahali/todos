9bb00a33d8539fd36f63cc57a54932f156f1826a
Fahd Rahali <rahali.fahd@gmail.com>
2022-04-27 00:05:22 +0100

    add a todolist component with its props

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,15 +1,43 @@
-import React from "react"
+import React  from "react";
+import Header from "./Header";
+import TodosList from './TodosList'
 class TodoContainer extends React.Component {
-    state = {
-        todos: []
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false
+      }
+    ]
+  };
+
+    handleChange = (id) => {
+      this.setState({
+        todos: this.state.todos.map((todo) => {
+          if (todo.id === id) todo.completed = !todo.completed;
+          return todo;
+        })
+      })
     }
+
   render() {
     return (
-      <React.StrictMode>
-        <h1>Hello from Create React App</h1>
-        <p>I am in a React Component!</p>
-      </React.StrictMode>
-    )
+      <div>
+      <Header /> 
+      <TodosList todos={this.state.todos}  handleChange={this.handleChange} />
+    </div>
+    );
   }
 }
-export default TodoContainer
+export default TodoContainer;
