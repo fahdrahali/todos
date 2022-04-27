f78838879263ecff00f048205a1f227ff07b1675
Fahd Rahali <rahali.fahd@gmail.com>
2022-04-28 00:50:16 +0100

    add an editing todo feature

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./TodoItem.module.css"
 
 
 
  const TodoItem = (props) => {
-    
+
+    const [state, setState] = useState({ editing: false});
+
     const completedStyle = {
         fontStyle: "italic",
         color: "#595959",
@@ -12,8 +14,29 @@ import styles from "./TodoItem.module.css"
         textDecoration: "line-through",
       }
 
+      const handleEditing = () => {
+        setState({ editing: true });
+      }
+
+     const handleUpdatedDone = event => {
+        if (event.key === "Enter") {
+            setState({ editing: false })
+          }
+      }
+
        const { title, completed, id } = props.todo;
+
+        let viewMode = {}
+        let editMode = {}
+
+        if (state.editing) {
+        viewMode.display = "none"
+        } else {
+        editMode.display = "none"
+        }
+
         return <li className={styles.item}> 
+        <div onClick={handleEditing}>
             <input 
             type='checkbox'
             className={styles.checkbox} 
@@ -24,7 +47,15 @@ import styles from "./TodoItem.module.css"
             <span style={completed ? completedStyle : null}>
             { title }
             </span>
-            </li>
+            <input type='text' className={styles.textInput}   style={editMode}
+            value={ title }
+            onChange={(e) => {
+                props.setUpdate(e.target.value, id)
+              }}
+            onKeyDown={handleUpdatedDone}
+            />
+         </div>
+        </li>
     }
  
 export default TodoItem;
