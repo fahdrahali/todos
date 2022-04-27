e75f8f11dfc36a073fd5025ebc0fc26f1adb049e
Fahd Rahali <rahali.fahd@gmail.com>
2022-04-27 02:08:49 +0100

    add a button delete

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -9,8 +9,8 @@ import React from 'react';
             checked={completed}
             onChange={() => props.handleChange(id)}
             /> 
+            <button onClick={() => props.handleDelete(id)}>Delete</button>
             { title }
-            
             </li>
     }
  
